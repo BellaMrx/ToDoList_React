60ed8fead246b73777257f7c0d140937049dbb58
BellaMrx <bella.mrx.pr0ject@gmail.com>
2023-12-20 22:03:30 +0100

    UD components

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # ToDoList_React
  ToDo list with React
+
+
+
+### install nanoid --> https://github.com/ai/nanoid
+- this is required so that each task returns a unique id
+
+- in the root directory (the folder in which your "react-project" is located)
+  ```
+ "$ cd react-project"
+  ```
+
+- then 
+  ```
+ npm install nanoid
+  ```
+
+if yarn:
+  ```
+ yarn add nanoid
+  ```
+
+- import nanoid to app.js  
+  ```
+ import { nanoid } from "nanoid";
+  ```
+
+
+
--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-function App() {
+
+
+function App(props) {
+  // to add a task
+  const [tasks, setTasks] = useState(props.tasks);
+
+  // for handling the form transmission for callbacks
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  
+  // a task is completed by clicking on the checkbox
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  // delete a task
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  
+  
+
+  // returns a <Todo /> component from map()function
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      // key is used to maintain an overview of tasks
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  // single or multiple task / counts/displays the task
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      {/* components/Form.js - <form> element to fill in a new task with <input type="text"> */}
+      <Form addTask={addTask} />
+      {/* components/FilterButton.js - an array of buttons to filter the tasks */}
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      {/* number of tasks still to be completed */}
+      <h2 id="list-heading">{headingText}</h2>
+      {/* components/Todo.js 3 tasks in unordered list, each <li> element has a checkbox and a button for editing and deleting */}
+      <ul role="list" className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
+          {/* <Todo name="Task1" completed={true} id="todo-0" /> */}
+          {/* completed={true/fasle} --> components/Todo.js - <input defaultChecked={props.completed} /> */}
+          {/* id="todo-0" --> components/Todo.js - <input id={props.id} /> */}
+        {taskList}
+      </ul>
     </div>
   );
 }
